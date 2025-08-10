dc7136524e01f839097220f6ac42e8369c63e01c
Lepros311 <andy.strickland@gmail.com>
2025-08-10 14:15:08 -0400

    Initial commit

---
+++ Ecommerce.Api/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.Api.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Name { get; set; }
+
+    public Category Category { get; set; }
+
+    public List<Sale> Sales { get; } = [];
+}
---
+++ Ecommerce.Api/Models/Sale.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Api.Models;
+
+public class Sale
+{
+    public int Id { get; set; }
+
+    public decimal Total { get; set; }
+
+    public List<Product> Products { get; } = [];
+}
